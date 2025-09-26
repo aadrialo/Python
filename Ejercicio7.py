5a2d8aa1ca8e9f2b80f8684b979a4706af2b58ee
aadrialo <aalopin187@ieszaidinvergeles.org>
2025-09-26 14:25:05 +0200

    Ejercicio7

---
+++ Ejercicio7.py
@@ -0,0 +1,4 @@
+var1= float(input("Jefe cuanto pesas?"))
+var2= float(input("Jefe cuanto mides?"))
+imc= (var1 / (var2)**2)
+print("Tu indice de masa corporal es "+ imc +"donde"+ imc +"es el indie de masa corporal calculado redondeando los dos decimales")
