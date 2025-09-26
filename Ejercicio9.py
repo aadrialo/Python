a82e254b3163471b81ed651c1f8141d6b12b7384
aadrialo <aalopin187@ieszaidinvergeles.org>
2025-09-26 14:40:42 +0200

    Ejercicio9 Correcto

---
+++ Ejercicio9.py
@@ -0,0 +1,6 @@
+inv= float(input("Cuanto quieres invertir?"))
+intereses=float(input("Tasa de interes?"))
+años=float(input("Cuantos años?"))
+
+intTot= inv*(intereses/100)*años
+print ("capital obtenido en la inversion: ", intTot)
