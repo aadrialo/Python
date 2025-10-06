4989a4826dbddf18b9cc14932c50125deb1e4faa
aadrialo <aalopin187@ieszaidinvergeles.org>
2025-10-06 12:23:08 +0200

    Subida Actividad 4  1.0

---
+++ Ejercicios de Tipos de Datos Simples AAP/Ejercicio 4.py
@@ -0,0 +1,5 @@
+#Escribir un programa que muestre por pantalla el resultado de la siguiente operación
+#aritmética
+operacion = float(((3+2)/(2*5))**2)
+operacionRes = str(operacion)
+print ("El resultado de la operacion es: " + operacionRes)
