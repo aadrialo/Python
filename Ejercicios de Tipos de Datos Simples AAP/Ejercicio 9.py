03e55ee9b3acab2725a202d440eb5b8874d5012f
aadrialo <aalopin187@ieszaidinvergeles.org>
2025-10-09 09:46:53 +0200

    Actualización Enunciado 9

--- Ejercicios de Tipos de Datos Simples AAP/Ejercicio 9.py
+++ Ejercicios de Tipos de Datos Simples AAP/Ejercicio 9.py
@@ -1,3 +1,5 @@
+#Escribir un programa que pregunte al usuario una cantidad a invertir, el interés anual
+#y el número de años, y muestre por pantalla el capital obtenido en la inversión.
 inv= float(input("Cuanto quieres invertir?"))
 intereses=float(input("Tasa de interes?"))
 años=float(input("Cuantos años?"))
