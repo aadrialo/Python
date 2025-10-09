23eda97a641ae590ca787438b45194d5f7b4ba65
aadrialo <aalopin187@ieszaidinvergeles.org>
2025-10-09 09:37:59 +0200

    Subida Actividad 10  1.0 + Actualizacion enunciado  7

---
+++ Ejercicios de Tipos de Datos Simples AAP/Ejercicio 10.py
@@ -0,0 +1,6 @@
+CuantosClowns = (int(input("Cuantos payasos quieres")))
+CuantosDolls = (int(input("Cuantas muñecas quieres")))
+clown = 112
+doll = 75
+pesoTotal = ((clown * CuantosClowns)+(doll * CuantosDolls))
+print("El peso total es de: ", pesoTotal, "g")
--- Ejercicios de Tipos de Datos Simples AAP/Ejercicio 7.py
+++ Ejercicios de Tipos de Datos Simples AAP/Ejercicio 7.py
@@ -1,3 +1,8 @@
+#Escribir un programa que pida al usuario su peso (en kg) y estatura (en metros),
+#calcule el índice de masa corporal y lo almacene en una variable, y muestre por
+#pantalla la frase Tu índice de masa corporal es <imc> donde <imc> es el
+#índice de masa corporal calculado redondeado con dos decimales.
+
 var1= float(input("Jefe cuanto pesas?"))
 var2= float(input("Jefe cuanto mides?"))
 imc= (var1 / (var2)**2)
