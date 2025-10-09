5d8ad9f3e90d864ebd2cfa5818924789b4dfc6fe
aadrialo <aalopin187@ieszaidinvergeles.org>
2025-10-09 08:54:19 +0200

    Subida Actividad 8  1.0

---
+++ Ejercicios de Tipos de Datos Simples AAP/Ejercicio 8.py
@@ -0,0 +1,6 @@
+var1 = int(input("Dame un numero entero")) 
+var2 = int(input("Dame otro numero entero")) 
+cociente = var1 / var2
+resto = var1 % var2
+
+print(var1 ,"entre", var2 ,"da un cociente", cociente ,"y un resto", resto ,"donde", var1 ," y ", var2 ,"son los números introducidos por el usuario, y", cociente ,"y", resto ,"son el cociente y el resto de la división entera respectivamente.")
