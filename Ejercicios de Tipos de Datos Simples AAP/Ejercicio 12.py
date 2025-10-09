5ac2102fb8d84dd2d0a85558868c22fe5245e271
aadrialo <aalopin187@ieszaidinvergeles.org>
2025-10-09 10:19:06 +0200

    Subida Actividad 12  1.0

---
+++ Ejercicios de Tipos de Datos Simples AAP/Ejercicio 12.py
@@ -0,0 +1,11 @@
+#Una panadería vende barras de pan a 3.49€ cada una. El pan que no es el día tiene
+#un descuento del 60%. Escribir un programa que comience leyendo el número de
+#barras vendidas que no son del día. Después el programa debe mostrar el precio
+#habitual de una barra de pan, el descuento que se le hace por no ser fresca y el
+#coste final total.
+
+noDay = int(input("¿Cuantas barras que no son del dia vendiste?"))
+normalPrice = float(3.49)
+PanPricenoDay = (3.49*0.6)
+TotalPrice= round((PanPricenoDay*noDay),2)
+print("El precio normal del pan es de ", normalPrice, "€, pero al no ser del dia se hace un 60% de descuento por lo que el precio de la barra de pan seria", PanPricenoDay, "€ y el precio total de las ganancias de esto panes ha sido de: ", TotalPrice,"€" )
