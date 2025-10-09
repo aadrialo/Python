b8ee7f4e1cc9b1caed28105e2815f26b4f2338d3
aadrialo <aalopin187@ieszaidinvergeles.org>
2025-10-09 09:44:42 +0200

    Actualización enunciado 8

--- Ejercicios de Tipos de Datos Simples AAP/Ejercicio 8.py
+++ Ejercicios de Tipos de Datos Simples AAP/Ejercicio 8.py
@@ -1,3 +1,7 @@
+#Escribir un programa que pida al usuario dos números enteros y muestre por
+#pantalla la <n> entre <m> da un cociente <c> y un resto <r> donde
+#<n> y <m> son los números introducidos por el usuario, y <c> y <r> son el cociente
+#y el resto de la división entera respectivamente.
 var1 = int(input("Dame un numero entero")) 
 var2 = int(input("Dame otro numero entero")) 
 cociente = var1 / var2
