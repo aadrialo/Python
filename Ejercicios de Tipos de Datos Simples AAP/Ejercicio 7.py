6fb34062a021c8976280d9a7c7d907506e7251e4
aadrialo <aalopin187@ieszaidinvergeles.org>
2025-10-09 08:31:33 +0200

    Subida Actividad 7   1.0

---
+++ Ejercicios de Tipos de Datos Simples AAP/Ejercicio 7.py
@@ -0,0 +1,5 @@
+var1= float(input("Jefe cuanto pesas?"))
+var2= float(input("Jefe cuanto mides?"))
+imc= (var1 / (var2)**2)
+imcRound= round(imc, 2)
+print("Tu indice de masa corporal es ", imc ," donde ", imcRound ," es el indie de masa corporal calculado redondeando los dos decimales")
