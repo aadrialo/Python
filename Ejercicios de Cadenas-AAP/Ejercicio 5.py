f30e798c4c5479ebc950bb42d1e16355b377dc52
aadrialo <aalopin187@ieszaidinvergeles.org>
2025-10-16 10:17:18 +0200

    Subida Actividad 5  1.0

---
+++ Ejercicios de Cadenas-AAP/Ejercicio 5.py
@@ -0,0 +1,5 @@
+#Escribir un programa que pida al usuario que introduzca una frase en la consola y
+#muestre por pantalla la frase invertida.
+frase = str(input("Escriba una frase: "))
+fraseReversa = frase[::-1]
+print (fraseReversa)
