33dcd39bd1ac1b48fc15996e3d153c402d151b4f
aadrialo <aalopin187@ieszaidinvergeles.org>
2025-10-23 10:59:15 +0200

    Subida Actividad 2  1.2

--- Ejercicios de Condicionales-AAP/Ejercicio 2.py
+++ Ejercicios de Condicionales-AAP/Ejercicio 2.py
@@ -2,11 +2,11 @@
 #variable, pregunte al usuario por la contraseña e imprima por pantalla si la
 #contraseña introducida por el usuario coincide con la guardada en la variable sin
 #tener en cuenta mayúsculas y minúsculas.
-contrasenia = str(input("Ingrese su contraseña: "))
-PassCheck = str (input("ingresa la contraseña otra vez: "))
-contraseniaL = contrasenia.lower()
-PassCheckL = PassCheck.lower()
 while True:
+    contrasenia = str(input("Ingrese su contraseña: "))
+    PassCheck = str (input("ingresa la contraseña otra vez: "))
+    contraseniaL = contrasenia.lower()
+    PassCheckL = PassCheck.lower()
     if (contraseniaL == PassCheckL):
         print("La contraseña es correcta")
         break
