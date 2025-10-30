efeca8b35b9b33bebb802e4cb3a4909f23941b57
aadrialo <aalopin187@ieszaidinvergeles.org>
2025-10-30 10:26:14 +0100

    Subida Actividad 1  1.0

---
+++ Ejercicios de Bucles - AAP/Ejercicio 1.py
@@ -0,0 +1,7 @@
+#Escribir un programa que pida al usuario una palabra y la muestre por pantalla 10
+#veces.
+palabra = str(input("Di 1 palabra: "))
+counter = 0
+while ( counter < 10):
+    print (palabra, "\n")
+    counter = counter + 1
