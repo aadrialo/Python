924b69649c29f66d5893bb92f9e17a4377947f74
aadrialo <aalopin187@ieszaidinvergeles.org>
2025-11-07 14:24:03 +0100

    Subida Actividad 1  1.0

---
+++ Ejercicios de Listas y Tuplas - AAP/Ejercicio 1.py
@@ -0,0 +1,8 @@
+#Escribir un programa que almacene las asignaturas de un curso (por ejemplo
+#Matemáticas, Física, Química, Historia y Lengua) en una lista y la muestre por
+#pantalla
+asignaturas = ["Matemáticas", "Física", "Química", "Historia", "Lengua"]
+cantidad = len(asignaturas)
+for i in range (0,cantidad):
+    print (asignaturas[i])
+
