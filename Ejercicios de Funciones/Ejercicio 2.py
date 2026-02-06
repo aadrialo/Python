6ba6a110761323f4aae66017f96ae8c55340944f
aadrialo <aalopin187@ieszaidinvergeles.org>
2026-02-06 09:24:04 +0100

    Subida Actividad 2  1.0

---
+++ Ejercicios de Funciones/Ejercicio 2.py
@@ -0,0 +1,8 @@
+#Escribir una función a la que se le pase una cadena <nombre> y muestre por
+#pantalla el saludo ¡hola <nombre>!.
+def hola(nombre):
+    print('¡Hola ' + nombre +'!')
+    return
+
+hola('paco')
+hola('marcos')
